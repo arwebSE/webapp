d77bbd27ab466a612b438b750734e7cbca2d3b4a
August R <aggesoft@gmail.com>
2022-05-09 21:38:44 +0200

    stock split file

--- screens/Stock.tsx
+++ screens/Stock.tsx
@@ -1,48 +1,8 @@
-import React, { useEffect, useState } from "react";
-import { useIsFocused } from "@react-navigation/native";
-import { Text, View, StyleSheet, FlatList, RefreshControl } from "react-native";
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
 
 import productModel from "../models/products";
-
-function StockList({ products, setProducts }) {
-    const [loading, setLoading] = useState<boolean>(false);
-
-    async function fetchProducts() {
-        setLoading(true);
-        setProducts(await productModel.getProducts());
-        setLoading(false);
-    }
-
-    useEffect(() => {
-        fetchProducts();
-    }, []);
-
-    const isFocused = useIsFocused();
-    useEffect(() => {
-        isFocused && fetchProducts();
-    }, [isFocused]);
-
-    return (
-        <FlatList
-            data={products}
-            renderItem={({ item, index }) => (
-                <View key={index} style={styles.container}>
-                    <Text style={styles.textLeft}>{item.name}</Text>
-                    <Text style={styles.textRight}>{item.stock}</Text>
-                </View>
-            )}
-            refreshControl={
-                <RefreshControl
-                    refreshing={loading}
-                    onRefresh={fetchProducts}
-                    colors={["black", "gray"]} // Android
-                    tintColor={"white"} // iOS
-                />
-            }
-            showsVerticalScrollIndicator={false}
-        />
-    );
-}
+import StockList from "../components/StockList";
 
 export default function Stock({ products, setProducts }) {
     return (
@@ -65,15 +25,5 @@ const styles = StyleSheet.create({
     container: {
         justifyContent: "space-between",
         flexDirection: "row",
-    },
-    textLeft: {
-        color: "#bbb",
-        fontSize: 18,
-        textAlign: "left",
-    },
-    textRight: {
-        color: "#bbb",
-        fontSize: 18,
-        textAlign: "right",
     },
 });
